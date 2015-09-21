abcfb81079b5b234b2e8df79278535727469ffa9
elpoisterio <rishabhyadav@aol.com>
2015-09-21 13:48:16 +0530

    first commit

---
+++ lexicon.py
@@ -0,0 +1,27 @@
+
+import urllib2
+import urllib
+import json
+
+
+def getFromJson(response):
+	jsonvalues = json.loads(response)  # jsonvalues is a dict
+	definations = jsonvalues["definitions"]
+	print "Meaning: "
+	for i in range(0,len(definations)):
+		print i+1,"." ,definations[i]["text"]
+
+
+
+## url for calling api
+url = "https://montanaflynn-dictionary.p.mashape.com/define?word=lexicon"
+# compulsory parameters for api 
+headers = { 'X-Mashape-Key' : "YKsdEFxI1umshEhDKjPwoq2ieMykp1SQM2BjsnREi06Uuf8wCD",
+			"Accept": "application/json" }
+url_values = urllib.urlencode(headers)	
+#sending request to server		
+request = urllib2.Request(url,headers=headers)
+response =urllib2.urlopen(request).read()
+#decoding json 
+getFromJson(response)
+
